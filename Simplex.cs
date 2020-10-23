a115c7e546f95a5b8d7a1a504df528883f78d84f
kymmie777 <38748168+kymmie777@users.noreply.github.com>
2020-10-23 21:14:00 +0300

    added partial class in namespace

---
+++ Simplex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simplex
+{
+    public partial class Simplex
+    {
+        private Expression _equation;
+        
+
+        public Simplex()
+        {
+            //InitializeComponent();
+        }
+    }
+}
