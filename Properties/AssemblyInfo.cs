1b1dbf9048cf64d17b5c296096af8583db729723
kymmie777 <38748168+kymmie777@users.noreply.github.com>
2021-02-10 17:44:00 +0300

    assembly

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+//General information about an assembly is controlled by the following set of attributes 
+[assembly: AssemblyTitle("Simplex")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simplex")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// If ComVisible to false, the types in this assembly will not be visible to COM components, set the ComVisible attribute to true for access of this assembly from COM.
+
+[assembly: ComVisible(false)]
+
+// GUID serves as the typelib identifier if this project is exposed to COM
+[assembly: Guid("AD428F7D-CAE7-4DE5-4A62-21A4F2ADAB57")]
+
+// version information for assembly:
+    //Major version //Minor version //Build number //Revision
+// specify all values ​​or defaults to build and revision numbers as in
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
